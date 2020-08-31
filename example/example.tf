c8ff86368b8e5c499fc6d5cf894f4638582572f8
bunnyshankar <bunny.shankar@gmail.com>
2020-08-31 14:29:56 +0000

    sample terraform script for creating an aws instance

---
+++ example/example.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "personal"
+  region = "ap-southeast-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0cd31be676780afa7"
+  instance_type = "t2.micro"
+}
